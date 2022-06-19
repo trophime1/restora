c9d03e7505c1be320a835e3ea647b152891f3671
trophime1 <trophime250@gmail.com>
2022-06-19 20:31:30 +0200

    app

--- src/App.js
+++ src/App.js
@@ -13,7 +13,46 @@ import Product from './pages/product';
 import Team from './pages/team';
 import Testimony from './testimony';
 import Footer from './pages/footer';
+import {useEffect} from 'react';
 function App() {
+  const scrollWithHash =() => {
+		if (window.location.hash) {
+			if (document.querySelector(window.location.hash)) {
+				let header = document.getElementById('header');
+				let offset = header.offsetHeight;
+				let elementPos = document.querySelector(window.location.hash).offsetTop;
+				if (!header.classList.contains('header-scrolled')) {
+					offset -= 10;
+				}
+
+				window.scrollTo({
+					top: elementPos - offset,
+					behavior: 'smooth'
+				});
+			}
+		}
+	}
+
+	useEffect(() => {
+		var c = Array.from(document.getElementsByTagName('a'));
+		c.forEach((element) => {
+			element.addEventListener('click', () => {
+				window.scroll(0, 0);
+			});
+		});
+
+		const script = document.createElement('script');
+		script.src = "/assets/js/main.js";
+		// script.src="assets/vendor/purecounter/purecounter.js";
+		script.async = true;
+		
+		document.body.appendChild(script);
+		scrollWithHash();
+
+		return () => {
+			document.body.removeChild(script);
+		}
+	}, []);
   return (<>
     <Top />
     <Navbar />
